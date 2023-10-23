285836533b8c19a4536094ff48210787212c7503
Luca Favini <luca.favini.02@gmail.com>
2023-10-23 23:17:24 +0200

    `OCaML`: ex7 `polishCalculator` partial implementation
    functors not working, polishcalculator stack not functional

---
+++ OCaML/Exercises/Lab1_2023-10-03/ex7_polishCalculator.ml
@@ -0,0 +1,104 @@
+#load "str.cma"
+
+module FunctionalStack (ElemType : sig type t end):
+  sig
+    type stack
+    val create : unit -> stack
+    val pop : stack -> (ElemType.t * stack)
+    val push : stack -> ElemType.t -> stack
+  end =
+  struct
+    exception Error
+
+    type stack =
+      | Empty
+      | List of ElemType.t list
+
+    let create () = Empty
+
+    let pop stack =
+      match stack with
+        | List (top :: []) -> (top, Empty)
+        | List (top :: rem) -> (top, List rem)
+        | _ -> raise Error
+
+    let push stack elem =
+      match stack with
+        | Empty -> List [elem]
+        | List list -> List (elem :: list)
+  end;;
+
+module PolishCalculator (Stack : FunctionalStack) :
+  sig
+    type expr
+    val expr_of_string : string -> expr
+    val eval : expr -> int
+  end =
+  struct
+    (* type expression, its final value (Value) or a subexpression (Expr) *)
+    type expr =
+      | Expr of expr * sign * expr
+      | Value of int
+    (* type sign, available operators (and Invalid, invalid operator) *)
+    and sign =
+      | Plus
+      | Minus
+      | Mult
+      | Div
+      | Pow
+      | Invalid
+
+    (* given a token (type string), return the correspondig constructor (type sign) *)
+    let parse_token token =
+      match token with
+        | "+" -> Plus
+        | "-" -> Minus
+        | "*" -> Mult
+        | "/" -> Div
+        | "**" -> Pow
+        | _ -> Invalid
+
+    (* parse str (type string) to an expression (type expr) *)
+    let expr_of_string str =
+      let rec expr_of_parsed_str str_list stack =
+        match str_list with
+          | [] -> Stack.pop stack
+          | token :: list ->
+            match (parse_token token) with
+              | Invalid -> (* Invalid = token is a a value, push to stack *)
+                  Stack.push (Value (int_of_string token)) stack;
+                  expr_of_parsed_str list stack
+              | _ -> (* token is an operator, pop last two expressions and create new expression *)
+                  (Stack.push (Expr ((Stack.pop stack), (parse_token token), (Stack.pop stack))) stack);
+                  expr_of_parsed_str list stack
+      (* split incoming string on spaces, initialize empty stack *)
+      in expr_of_parsed_str (Str.split (Str.regexp " ") str) (Stack.create ())
+
+    (* parse operator (type sign) into a function that operates on two integers (type fun int int -> int) *)
+    let parse_op op =
+      (* power function *)
+      let pow num exp =
+        let rec pow num exp res =
+          match exp with
+            | 1 -> res
+            | _ -> pow num (exp-1) (res*num)
+        in pow num exp num
+      in match op with
+        | Plus -> (+)
+        | Minus -> (-)
+        | Mult -> ( * )
+        | Div -> (/)
+        | Pow -> pow
+        | _ -> (+)
+
+    (* evaluate expression (type expr) to its final value (type integer) *)
+    let eval expr =
+      let rec eval expr =
+        match expr with
+          | Value i -> i
+          | Expr (e1, op, e2) -> (parse_op op) (eval e1) (eval e2)
+      in eval expr
+  end;;
+
+PolishCalculator.eval (PolishCalculator.expr_of_string "10 2 + 4 - 3 * 2 / 2 **");;
+PolishCalculator.eval (PolishCalculator.expr_of_string "25");;
